7f7ee3695039133335140d24d23240dcb4e8bda5
Ekaterina <eskanevaa@gmail.com>
2025-05-26 20:17:45 +0300

    добавила мини-игру 🐞

--- js.js
+++ js.js
@@ -5,3 +5,18 @@ const button = document.querySelector('button');
 button.addEventListener('click', function() {
 	alert('Стабильность — это иллюзия. Проверим ещё раз?')
 })
+
+const bug = document.querySelector('.bug-link');
+let clickedOnce = false;
+
+// Проверяем, что устройство без hover (мобильное)
+if (window.matchMedia('(hover: none)').matches) {
+  bug.addEventListener('click', (e) => {
+    if (!clickedOnce) {
+      e.preventDefault(); // чтобы первый клик не переходил по ссылке
+      alert("Я в прод. Поймаешь меня? (еще 1 клик)");
+      clickedOnce = true;
+      setTimeout(() => clickedOnce = false, 9000); 
+    }
+  });
+}
